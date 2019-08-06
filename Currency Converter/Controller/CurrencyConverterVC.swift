394afe15ef16aac1e41baf64089f3a6b770b8363
Henry Chukwu <henry.chukwu@andela.com>
2019-08-06 21:23:14 +0100

    add gitignore file and implement latest conversion api

--- Currency Converter/Controller/CurrencyConverterVC.swift
+++ Currency Converter/Controller/CurrencyConverterVC.swift
@@ -18,15 +18,18 @@ class CurrencyConverterVC: UIViewController {
     @IBOutlet weak var targetCurrencyView: UIView!
     @IBOutlet weak var targetCurrencyTextField: UITextField!
     @IBOutlet weak var targetCurrencyLabel: UILabel!
-    @IBOutlet weak var baseCurrencybottomView: UIView!
+    @IBOutlet weak var baseCurrencyButtomView: UIView!
+    @IBOutlet weak var baseCurrencyButton: UIButton!
     @IBOutlet weak var baseCurrencyFlag: UIImageView!
-    @IBOutlet weak var baseCurrencybuttomLbl: UILabel!
+    @IBOutlet weak var baseCurrencyButtomLbl: UILabel!
+    @IBOutlet weak var targetCurrencyButtomView: UIView!
+    @IBOutlet weak var targetCurrencyButton: UIButton!
     @IBOutlet weak var targetCurrencyFlag: UIImageView!
     @IBOutlet weak var targetCurrencyButtomLbl: UILabel!
     @IBOutlet weak var convertButton: UIButton!
     @IBOutlet weak var marketExchangeRateBtn: UIButton!
     @IBOutlet weak var marketExchangeHelpImageView: UIImageView!
-    @IBOutlet weak var ButtomUiView: UIView!
+    @IBOutlet weak var buttomUIView: UIView!
 
     fileprivate let presenter = CurrencyConverterPresenter()
     fileprivate var activity: UIActivityIndicatorView?
@@ -35,13 +38,64 @@ class CurrencyConverterVC: UIViewController {
         super.viewDidLoad()
 
         presenter.attachView(self)
+        setupView()
+    }
+
+    private func setupView() {
+        roundedLabel.layer.cornerRadius = roundedLabel.frame.width / 2
+        roundedLabel.layer.masksToBounds = true
+        roundedLabel.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+        roundedLabel.layer.shadowOffset = CGSize(width: 2, height: 2)
+        roundedLabel.layer.shadowOpacity = 0.8
+        baseCurrencyView.layer.cornerRadius = 5
+        baseCurrencyButtomView.layer.cornerRadius = 3
+        baseCurrencyButtomView.layer.borderWidth = 1
+        baseCurrencyButtomView.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        targetCurrencyView.layer.cornerRadius = 5
+        targetCurrencyButtomView.layer.cornerRadius = 3
+        targetCurrencyButtomView.layer.borderWidth = 1
+        targetCurrencyButtomView.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        convertButton.layer.cornerRadius = 5
+        buttomUIView.layer.cornerRadius = 20
+
+        if let baseCountryFlag = baseCurrencyButtomLbl.text {
+            flag(baseCountry: baseCountryFlag)
+        }
+
+    }
+
+    private func flag(baseCountry: String) {
+//        let base: UInt32 = 127397
+//        var baseString = ""
+//        var targetString = ""
+//        var baseCountryScalar = baseCountry.unicodeScalars.makeIterator()
+//        var targetCountryScalar = targetCountry.unicodeScalars.makeIterator()
+//
+//        while let v1 = baseCountryScalar.next(), let v2 = targetCountryScalar.next() {
+//            if let baseValue = Unicode.Scalar(base + v1.value), let targetValue = Unicode.Scalar(base + v2.value) {
+//                baseString.unicodeScalars.append(baseValue)
+//                targetString.unicodeScalars.append(targetValue)
+//            }
+//        }
+
+        let base : UInt32 = 127397
+        var s = ""
+        for v in baseCountry.unicodeScalars {
+            s.unicodeScalars.append(UnicodeScalar(base + v.value)!)
+        }
+
+        baseCurrencyFlag.image = UIImage(named: s)
+//        targetCurrencyFlag.image = UIImage(named: String(targetString))
     }
 
     @IBAction func convertBtnWasPressed(_ sender: Any) {
+        guard let currency = targetCurrencyLabel.text else { return }
+        presenter.getConversion(target: currency)
     }
 
     @IBAction func signUpBtnWasPressed(_ sender: Any) {
     }
+
     @IBAction func toggleBtnWasPressed(_ sender: Any) {
     }
     
